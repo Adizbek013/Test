94ef59d395fbc504df7ca61c91be415dd0c957b9
Karimov A <119536660+Adizbek013@users.noreply.github.com>
2023-10-25 22:03:54 +0500

    first commit

--- web_scrapping.py
+++ web_scrapping.py
@@ -1,24 +1,24 @@
 from playwright.sync_api import sync_playwright
+import os
 
 
-def kun_uz():
-    url = 'https://kun.uz/news/category/jahon'
-    with sync_playwright() as play:
-        browser = play.firefox.launch(headless=True)
+def play(username, password):
+    with sync_playwright() as playwright:
+        browser = playwright.firefox.launch(headless=False)
         context = browser.new_context()
         page = context.new_page()
-        page.goto(url)
-        r = page.query_selector_all('.top-news__big')
-        page.screenshot(path=f'example-{play.firefox.name}.png')
-        for i in r:
-            info1 = i.text_content().split()[2][10:]
-            info2 = i.text_content().split()[3:]
-            year = i.text_content().split()[2][0:10]
-            date = i.text_content().split()[0]
-        data_dict = {
-            "date": date,
-            "year": year,
-            "info1": info1,
-            "info2": info2
-        }
-    return data_dict
+        page.goto('https://www.instagram.com/accounts/login/?source=auth_switcher')
+        page.get_by_label("Phone number, username, or email").fill(username)
+        page.get_by_label("Password").fill(password)
+        page.get_by_role('button', name='Log in', exact=True).click()
+        page.wait_for_timeout(3000)
+        page.get_by_alt_text(f"{username}'s profile picture").click()
+        page.get_by_role("link", name="Clip").click()
+        page.wait_for_timeout(1000)
+        page.screenshot(path='post_one.png')
+        browser.close()
+
+
+username = os.getenv('USERNAME')
+password = os.getenv('PASSWORD')
+play(username, password)
